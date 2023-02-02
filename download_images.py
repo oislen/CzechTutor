83114f58a677459ddadf22114ac6fd2bdc6e7a85
Oisin <oisin.leonard@gmail.com>
2023-02-02 10:38:26 +0000

    created script to download images with

---
+++ download_images.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import cons
+from scripts.utilities.webscraper import main
+
+# if running as main programme
+if __name__ == '__main__':
+    # load image urls from data
+    data_objs = [pd.read_excel(cons.vocab_fpath, sheet_name = topic, usecols=['Image']) for topic in cons.topics]
+    data = pd.concat(objs = data_objs, axis = 0, ignore_index = True)
+    # scrape url images
+    main(srcs = data['Image'].to_list(), output_dir = cons.images_dir)
