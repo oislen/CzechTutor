6cd49f1b9179f4af271ef48cf5474c8afb26d54e
Oisin <oisin.leonard@gmail.com>
2023-02-02 10:39:07 +0000

    added directory paths

--- cons.py
+++ cons.py
@@ -1,10 +1,15 @@
 import os
 
+# set directory names
 git_dir = 'E:\\GitHub'
 root_dir = os.path.join(git_dir, 'CzechTutor')
-scripts_dir = os.path.join(git_dir, 'scripts')
-data_dir = os.path.join(git_dir, 'data')
-vocab_fpath = 'C:\\Users\\oisin\\Documents\\Czech\\vocab.xlsx'
+scripts_dir = os.path.join(root_dir, 'scripts')
+data_dir = os.path.join(root_dir, 'data')
+images_dir = os.path.join(data_dir, 'images')
+# set file names
+vocab_fpath = os.path.join(data_dir, 'vocab.xlsx')
+# set available topics
 #topics = ['Animals', 'Countryside', 'City', 'Drinks', 'Food', 'House', 'Hobbies', 'People', 'School']
 topics = ['Animals']
+# set free-images url
 url = 'https://free-images.com'
