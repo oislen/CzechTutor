a753530c5f5d022e4c20b4a959a78697e6773246
Oisin <oisin.leonard@gmail.com>
2023-02-04 15:41:58 +0000

    Added functionality to exit at point of topic selection

--- main.py
+++ main.py
@@ -16,28 +16,34 @@ def main(run = True, questions = 10, n_options = 4):
     while run:
         # select vocabulary topic
         topic = select_topic()
-        # set total to zero
-        total = 0
-        # read in Czech vocabulary data
-        data = pd.read_excel(cons.vocab_fpath, sheet_name=topic)
-        # loop over each question
-        for i in range(questions):
-            # pull data for the questions
-            target_sample, outside_samples, options = pull_data(data, n_options)
-            # extract english translation of sample
-            english = target_sample['English'].iloc[0]
-            # determine form of question
-            form = select_form()
-            # plot image of english word
-            plot_english(target_sample['Image'].iloc[0])
-            # pose question
-            valid_options = formulate_question(english, form, options)
-            # process user's answer
-            answer = process_user_answer(valid_options)
-            # grade answer
-            total = grade_answer(answer, target_sample, form, options, english, total)  
-        # print results
-        print(f'{total} out of {questions} questions correct')
+        # if topic is not exit
+        if topic != 'exit':
+            # set total to zero
+            total = 0
+            # read in Czech vocabulary data
+            data = pd.read_excel(cons.vocab_fpath, sheet_name=topic)
+            # loop over each question
+            for i in range(questions):
+                # pull data for the questions
+                target_sample, outside_samples, options = pull_data(data, n_options)
+                # extract english translation of sample
+                english = target_sample['English'].iloc[0]
+                # determine form of question
+                form = select_form()
+                # plot image of english word
+                plot_english(target_sample['Image'].iloc[0])
+                # pose question
+                valid_options = formulate_question(english, form, options)
+                # process user's answer
+                answer = process_user_answer(valid_options)
+                # grade answer
+                total = grade_answer(answer, target_sample, form, options, english, total)  
+            # print results
+            print(f'{total} out of {questions} questions correct')
+        # otherwise
+        else:
+            # set run to False
+            run = False
     return 0
 
 
