f953b9b01f600b3f73756c5d3cd76758b7f2b941
Oisin <oisin.leonard@gmail.com>
2023-02-04 21:34:07 +0000

    appending directories to path

--- cons.py
+++ cons.py
@@ -1,9 +1,11 @@
 import os
+import sys
 
 # set directory names
 git_dir = 'E:\\GitHub'
 root_dir = os.path.join(git_dir, 'CzechTutor')
 scripts_dir = os.path.join(root_dir, 'scripts')
+utilities_dir = os.path.join(root_dir, 'utilities')
 data_dir = os.path.join(root_dir, 'data')
 images_dir = os.path.join(data_dir, 'images')
 # set file names
@@ -12,3 +14,7 @@ vocab_fpath = os.path.join(data_dir, 'vocab.xlsx')
 topics = ['Animals', 'City', 'Countryside', 'Drinks']
 # set free-images url
 url = 'https://free-images.com'
+# combine all directories into one list
+dir_list = [git_dir, root_dir, scripts_dir, utilities_dir, data_dir, images_dir]
+for dir in dir_list:
+    sys.path.append(dir)
