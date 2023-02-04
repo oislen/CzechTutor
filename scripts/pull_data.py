aaaf4dade9d19f4cb01b67d21df5529839f18639
Oisin <oisin.leonard@gmail.com>
2023-02-04 15:36:04 +0000

    Added n_options parameter to control how many answer options to retrieve from the data

--- scripts/pull_data.py
+++ scripts/pull_data.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-def pull_data(data):
+def pull_data(data, n_options):
     """"""
     # random sample a record from the data
     target_sample = data.sample(n = 1)
     # extract three other records from the data
-    outside_samples = data.loc[data.index.drop(target_sample.index[0]), :].sample(n = 3)
+    outside_samples = data.loc[data.index.drop(target_sample.index[0]), :].sample(n = n_options - 1)
     # randomise samples
-    options = pd.concat(objs = [target_sample, outside_samples], axis = 0).sample(n = 4, replace = False)
+    options = pd.concat(objs = [target_sample, outside_samples], axis = 0).sample(n = n_options, replace = False)
     return target_sample, outside_samples, options
