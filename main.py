92f452c555d485f17f33ca05e38e3a6da557d9c3
Oisin <oisin.leonard@gmail.com>
2023-02-04 21:34:35 +0000

    Restructured individual functions into a single function

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-import cons
 import pandas as pd
-
-from scripts.utilities.plot_english import plot_english
-
-from scripts.pull_data import pull_data
+# load in scripts
+import cons
 from scripts.select_topic import select_topic
-from scripts.select_form import select_form
-from scripts.formulate_question import formulate_question
-from scripts.process_user_answer import process_user_answer
-from scripts.grade_answer import grade_answer
+from scripts.question_pipeline import question_pipeline
 
-def main(run = True, questions = 10, n_options = 4):
+def main(run = True, n_questions = 10, n_options = 4):
     """
     """
     while run:
@@ -23,23 +17,13 @@ def main(run = True, questions = 10, n_options = 4):
             # read in Czech vocabulary data
             data = pd.read_excel(cons.vocab_fpath, sheet_name=topic)
             # loop over each question
-            for i in range(questions):
-                # pull data for the questions
-                target_sample, outside_samples, options = pull_data(data, n_options)
-                # extract english translation of sample
-                english = target_sample['English'].iloc[0]
-                # determine form of question
-                form = select_form()
-                # plot image of english word
-                plot_english(target_sample['Image'].iloc[0])
-                # pose question
-                valid_options = formulate_question(english, form, options)
-                # process user's answer
-                answer = process_user_answer(valid_options)
-                # grade answer
-                total = grade_answer(answer, target_sample, form, options, english, total)  
+            for i in range(n_questions):
+                # create a dictionary to contain question data
+                quest_dict = {'n_options':n_options, 'data':data}
+                # run question pipeline
+                quest_dict, total = question_pipeline(quest_dict, total)
             # print results
-            print(f'{total} out of {questions} questions correct')
+            print(f'{total} out of {n_questions} questions correct')
         # otherwise
         else:
             # set run to False
