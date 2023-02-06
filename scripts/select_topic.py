e195145e6d9670f0a25f2cf4269a80cf81d15d51
Oisin <oisin.leonard@gmail.com>
2023-02-06 18:09:03 +0000

    Formatted topic selection to display as bullet points

--- scripts/select_topic.py
+++ scripts/select_topic.py
@@ -6,10 +6,12 @@ def select_topic():
     topic_selected = 0
     # while no topic has been selected
     while topic_selected == 0:
+        # format topics into bullet points
+        topics_str = ' '.join([f'* {topic} \n' for topic in cons.topics])
         # ask for user input
-        topic = input(f'Select a Topic: {cons.topics}, or exit \n')
+        topic = input(f'Select a Topic: \n\n {topics_str} \n Or Exit \n \n').strip()
         # verify user input
-        user_input, return_code = check_user_input(topic, cons.topics + ['exit'])
+        user_input, return_code = check_user_input(topic, cons.topics + ['Exit'])
         # if a valid topic has been selected
         if return_code == 0:
             # update topic selected parameter
