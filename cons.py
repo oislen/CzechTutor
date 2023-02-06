48902a8b359afde8900b65d1d6d61e1040ae531d
Oisin <oisin.leonard@gmail.com>
2023-02-06 18:07:25 +0000

    Added clothes to available topics

--- cons.py
+++ cons.py
@@ -13,7 +13,7 @@ images_dir = os.path.join(data_dir, 'images')
 # set file names
 vocab_fpath = os.path.join(data_dir, 'vocab.xlsx')
 # set available topics
-topics = ['Animals', 'City', 'Countryside', 'Drinks', 'Food', 'House', 'Hobbies', 'People', 'Numbers', 'School']
+topics = ['Animals', 'Clothes', 'City', 'Countryside', 'Drinks', 'Food', 'House', 'Hobbies', 'People', 'Numbers', 'School']
 # set free-images url
 url = 'https://free-images.com'
 # combine all directories into one list
