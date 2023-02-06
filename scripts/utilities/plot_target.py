5ee4865ca11e1a666b1d0e2d272dccfa7a7b54b3
Oisin <oisin.leonard@gmail.com>
2023-02-06 15:10:18 +0000

    Added if else to only plot image if image url exists

--- scripts/utilities/plot_target.py
+++ scripts/utilities/plot_target.py
@@ -13,17 +13,20 @@ def plot_target(quest_dict, plot_from = 'disk'):
     options = quest_dict['options']
     # extract image url
     image_url = options.loc[options['target'] == 1, 'Image'].iloc[0]
-    # if plotting from disk
-    if plot_from == 'disk':
-        image_stem = image_url.split('/')[-1]
-        image_path = os.path.join(cons.images_dir, image_stem)
-        img = mpimg.imread(image_path)
-    # else plotting from url
-    elif plot_from == 'url':
-        image_page = requests.get(image_url)
-        img = Image.open(BytesIO(image_page.content))
-    # plot image
-    plt.imshow(img)
-    plt.axis('off')
-    plt.show()
+    # if image url exists
+    if image_url == image_url:
+        # if plotting from disk
+        if plot_from == 'disk':
+            image_stem = image_url.split('/')[-1]
+            image_path = os.path.join(cons.images_dir, image_stem)
+            img = mpimg.imread(image_path)
+        # else plotting from url
+        elif plot_from == 'url':
+            image_page = requests.get(image_url)
+            img = Image.open(BytesIO(image_page.content))
+        # plot image
+        plt.figure(figsize=(10, 8))
+        plt.imshow(img)
+        plt.axis('off')
+        plt.show()
     return 0
