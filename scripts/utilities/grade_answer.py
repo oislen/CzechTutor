00e1d93b3092a04a5f8d54168b85c791a578cae6
Oisin <oisin.leonard@gmail.com>
2023-02-06 18:09:51 +0000

    Added topic_dict to persist total

--- scripts/utilities/grade_answer.py
+++ scripts/utilities/grade_answer.py
@@ -1,4 +1,4 @@
-def grade_answer(quest_dict, total):
+def grade_answer(quest_dict, topic_dict):
     """
     """
     # extract records from question dictionary
@@ -6,17 +6,22 @@ def grade_answer(quest_dict, total):
     form = quest_dict['form']
     answer = quest_dict['answer']
     english = quest_dict['english']
+    total = topic_dict['total']
     # extract english translation of sample
     czech = options.loc[options['target'] == 1, form].iloc[0]
     # retrieve the english translation of the answer
     answer_english = options.loc[options[form] == answer, 'English'].iloc[0]
-    # determine if the correct answer was choosen
+    # retrieve answer wikitionary link
+    answer_wiktionary = options.loc[options[form] == answer, 'Wiktionary'].iloc[0]
+    # determine if the correct answer was chosen
     result = 1 if answer_english == english else 0
     # update running total of game
-    total = total + result
+    topic_dict['total'] = total + result
     # generate feedback response
     response = 'Correct' if result == 1 else f"Incorrect, the correct answer was {czech}"
     # assign to dictionary
+    quest_dict['result'] = result
     quest_dict['response'] = response
+    quest_dict['total'] = total
     print(response)
-    return quest_dict, total
+    return quest_dict, topic_dict
