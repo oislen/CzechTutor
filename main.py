2a687f7f659bb30bd0b4450bcbe878665b73f0b6
Oisin <oisin.leonard@gmail.com>
2023-02-06 18:08:03 +0000

    Added topic_dict object to track total and questions across for loop

--- main.py
+++ main.py
@@ -11,22 +11,27 @@ def main(run = True, n_questions = 10, n_options = 4):
         # select vocabulary topic
         topic = select_topic()
         # if topic is not exit
-        if topic != 'exit':
-            # set total to zero
-            total = 0
+        if topic != 'Exit':
+            # create topic_dict
+            topic_dict = {}
+            # set total to zero, and feedback string to ''
+            topic_dict['total'] = 0
             # read in Czech vocabulary data
             data = pd.read_excel(cons.vocab_fpath, sheet_name=topic)
             # loop over each question
-            for i in range(n_questions):
+            for idx in range(n_questions):
                 # create a dictionary to contain question data
                 quest_dict = {'n_options':n_options, 'data':data}
                 # run question pipeline
-                quest_dict, total = question_pipeline(quest_dict, total)
+                quest_dict, topic_dict = question_pipeline(quest_dict, topic_dict)
+                # assign question dictionary to topic dictionary
+                topic_dict[f'quest_dict_{idx}'] = quest_dict
+            # TODO: add function to generate topic feedback here
             # print results
-            print(f'{total} out of {n_questions} questions correct')
+            print(f'{topic_dict["total"]} out of {n_questions} questions correct')
         # otherwise
         else:
-            # set run to False
+            # set run to False, and exit programme
             run = False
     return 0
 
