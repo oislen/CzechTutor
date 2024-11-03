1dd81091e92c0d1921081a6e34e3a4f16c2fecea
Oisin <oisin.leonard@gmail.com>
2024-11-03 20:37:45 +0000

    Updated recordSet name

--- scripts/WordsInArray.java
+++ scripts/WordsInArray.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -14,7 +12,7 @@ public class WordsInArray {
             FileInputStream fis = new FileInputStream("E:\\GitHub\\CzechTutor\\data\\ces-eng\\ces.txt");
             InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
             BufferedReader buf = new BufferedReader(isr);
-            ArrayList<Map<String, String>> words = new ArrayList<>();
+            ArrayList<Map<String, String>> recordSet = new ArrayList<>();
             String lineJustFetched = null;
             
 
@@ -23,18 +21,18 @@ public class WordsInArray {
                 if(lineJustFetched == null){  
                     break; 
                 }else{
-                    String[] arrayValues;
                     String[] arrayKeys = {"EN", "CZ", "REF"}; 
+                    String[] arrayValues;
                     arrayValues = lineJustFetched.split("\t");
                     Map<String, String> mapObject = IntStream.range(0, arrayKeys.length).boxed().collect(Collectors.toMap(i -> arrayKeys[i], i -> arrayValues[i]));
-                    words.add(mapObject);
+                    recordSet.add(mapObject);
                 }
             }
 
 
             buf.close();
 
-            System.out.println(words);
+            System.out.println(recordSet);
 
         }catch(Exception e){
             e.printStackTrace();
