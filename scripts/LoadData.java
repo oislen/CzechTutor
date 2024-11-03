c015808aed54767d9afd47f8598e091938f2101c
Oisin <oisin.leonard@gmail.com>
2024-11-03 21:00:48 +0000

    Updated module name

--- scripts/LoadData.java
+++ scripts/LoadData.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class WordsInArray {
+public class LoadData {
     public static void main(String[] args) {
         try{
             FileInputStream fis = new FileInputStream("E:\\GitHub\\CzechTutor\\data\\ces-eng\\ces.txt");
