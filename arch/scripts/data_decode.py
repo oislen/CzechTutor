5465a916dbb462aca4ac098dd285165cea050183
Oisin <oisin.leonard@gmail.com>
2024-11-04 21:39:53 +0000

    Writing without index

--- arch/scripts/data_decode.py
+++ arch/scripts/data_decode.py
@@ -4,4 +4,4 @@ data = pd.read_csv("E:\\GitHub\\CzechTutor\\data\\ces-eng\\ces.txt", sep="\t", h
 data[0] = data[0].apply(lambda x: unidecode(x))
 data[1] = data[1].apply(lambda x: unidecode(x))
 data[2] = data[2].apply(lambda x: unidecode(x))
-data.to_csv("E:\\GitHub\\CzechTutor\\data\\ces-eng\\ces_bkp.txt", sep="\t", header=None, encoding="utf-8")
+data.to_csv("E:\\GitHub\\CzechTutor\\data\\ces-eng\\ces_bkp.txt", sep="\t", header=None, encoding="utf-8", index=False)
