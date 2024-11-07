19d9bcc11205904df44cfd5627114d2c9eaa5781
Oisin <oisin.leonard@gmail.com>
2024-11-07 21:34:31 +0000

    Added min and max tests for randomIndices

--- src/test/java/com/czechtutor/Choose_Test.java
+++ src/test/java/com/czechtutor/Choose_Test.java
@@ -1,6 +1,7 @@
 package com.czechtutor;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,18 @@ public class Choose_Test {
     void randomIndices_ArrayLength_Test() {
         ArrayList<Integer> indexArray = Choose.randomIndices(2, 5);
         Assertions.assertEquals(2, indexArray.size());
+    }
+
+    @Test
+    void randomIndices_ArrayMax_Test() {
+        ArrayList<Integer> indexArray = Choose.randomIndices(2, 5);
+        Assertions.assertTrue(Collections.max(indexArray) <= 5);
+    }
+    
+    @Test
+    void randomIndices_ArrayMin_Test() {
+        ArrayList<Integer> indexArray = Choose.randomIndices(2, 5);
+        Assertions.assertTrue(Collections.min(indexArray) >= 0);
     }
 
 }
