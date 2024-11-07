6d33e7cef2d850acd8c068d318641e66e122983a
Oisin <oisin.leonard@gmail.com>
2024-11-07 20:49:26 +0000

    Calling java app with maven

--- exeMain.bat
+++ exeMain.bat
@@ -1,2 +1,3 @@
-call javac src\\main\\java\\com\\czechtutor\\Main.java
-call java -cp src\\main\\java com.czechtutor.Main
+:: call javac src\\main\\java\\com\\czechtutor\\Main.java
+:: call java -cp src\\main\\java com.czechtutor.Main
+call mvn exec:java -Dexec.mainClass="com.czechtutor.Main"
