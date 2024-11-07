3c14ca819d12892b10a58d592355f8c7f30b0cfd
Oisin <oisin.leonard@gmail.com>
2024-11-07 21:38:51 +0000

    Parameterised nIndices and upperIndexBound

--- src/test/java/com/czechtutor/Choose_Test.java
+++ src/test/java/com/czechtutor/Choose_Test.java
@@ -8,21 +8,24 @@ import org.junit.jupiter.api.Test;
 
 public class Choose_Test {
     
+    Integer nIndices = 2;
+    Integer upperIndexBound = 5;
+
     @Test
     void randomIndices_ArrayLength_Test() {
-        ArrayList<Integer> indexArray = Choose.randomIndices(2, 5);
+        ArrayList<Integer> indexArray = Choose.randomIndices(nIndices, upperIndexBound);
         Assertions.assertEquals(2, indexArray.size());
     }
 
     @Test
     void randomIndices_ArrayMax_Test() {
-        ArrayList<Integer> indexArray = Choose.randomIndices(2, 5);
-        Assertions.assertTrue(Collections.max(indexArray) <= 5);
+        ArrayList<Integer> indexArray = Choose.randomIndices(nIndices, upperIndexBound);
+        Assertions.assertTrue(Collections.max(indexArray) <= upperIndexBound);
     }
     
     @Test
     void randomIndices_ArrayMin_Test() {
-        ArrayList<Integer> indexArray = Choose.randomIndices(2, 5);
+        ArrayList<Integer> indexArray = Choose.randomIndices(nIndices, upperIndexBound);
         Assertions.assertTrue(Collections.min(indexArray) >= 0);
     }
 
