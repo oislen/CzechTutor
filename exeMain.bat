f6b3b163496fc5e21aa9a7fcf1cd51b19c232cfb
Oisin <oisin.leonard@gmail.com>
2024-11-07 21:43:39 +0000

    Added comment for running maven tests

--- exeMain.bat
+++ exeMain.bat
@@ -1,3 +1,4 @@
 :: call javac src\\main\\java\\com\\czechtutor\\Main.java
 :: call java -cp src\\main\\java com.czechtutor.Main
 call mvn exec:java -Dexec.mainClass="com.czechtutor.Main"
+:: call mvn test
