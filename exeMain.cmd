a51922400fffa49c0f6ae948ae503d81da45281d
Oisin <oisin.leonard@gmail.com>
2024-11-08 08:39:25 +0000

    Added maven commands to .cmd file

--- exeMain.cmd
+++ exeMain.cmd
@@ -1,4 +1,6 @@
 :: call javac src\\main\\java\\com\\czechtutor\\Main.java
 :: call java -cp src\\main\\java com.czechtutor.Main
-call mvn exec:java -Dexec.mainClass="com.czechtutor.Main"
-:: call mvn test
+call mvnw exec:java -Dexec.mainClass="com.czechtutor.Main"
+:: call mvnw compile test
+:: call mvnw clean compile test package
+:: call mvnw clean install
