9f4b1529273398209c46c70512030c46d0d4bcc1
Oisin <oisin.leonard@gmail.com>
2024-11-10 10:04:06 +0000

    Updated exe file

--- server/exeMain.cmd
+++ server/exeMain.cmd
@@ -1,6 +1,6 @@
 :: call javac src\\main\\java\\com\\czechtutor\\Main.java
 :: call java -cp src\\main\\java com.czechtutor.Main
-call mvnw exec:java -Dexec.mainClass="com.czechtutor.Lesson"
+call mvnw exec:java -Dexec.mainClass="com.czechtutor.Main"
 :: call mvnw compile test
 :: call mvnw clean compile test package
 :: call mvnw clean install
