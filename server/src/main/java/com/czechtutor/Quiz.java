7f9050e6ba0545872d94f26221b7651e5d7a5979
Oisin <oisin.leonard@gmail.com>
2024-11-15 08:55:15 +0000

    Added logic for initialising questionId

--- server/src/main/java/com/czechtutor/Quiz.java
+++ server/src/main/java/com/czechtutor/Quiz.java
@@ -18,14 +18,24 @@ public class Quiz {
         lessonPayload.put("nOptions", nOptions);
         Lesson lesson = new Lesson();
         lesson.set(lessonPayload);
-        // load czech / english language phrases from disk
+        // create an array for holding the questions
         ArrayList<Question> quiz = new ArrayList<>();
+        // set the initial questionId
+        Integer questionId = 1;
+        // load czech / english language phrases from disk
         ArrayList<HashMap<String, String>> recordSet = Data.load("E:\\GitHub\\CzechTutor\\server\\src\\main\\resources\\ces_bkp.txt");
-        for (int questionIndex = 0; questionIndex<nQuestions; questionIndex++) {
+        for (int questionSubId = 1; questionSubId<=nQuestions; questionSubId++) {
             // create question payload
+            HashMap<String, Object> questionPayload;
+            questionPayload = lesson.getHashMap();
+            questionPayload.put("questionId", questionId);
+            questionPayload.put("questionSubId", questionSubId);
+            // set question
             Question question = new Question();
-            question.set(lesson.getHashMap(), recordSet);
+            question.set(questionPayload, recordSet);
             quiz.add(question);
+            // increment questionId
+            questionId++;
         }
         return quiz;
 
