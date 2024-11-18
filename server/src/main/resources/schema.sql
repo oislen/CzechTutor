ad5abeb9fa6c5370837abaa583dd63a89bb31364
Oisin <oisin.leonard@gmail.com>
2024-11-18 12:18:44 +0000

    Created schema for initialising h2 lesson table

---
+++ server/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table if not exists lesson (
+    lesson_id bigint primary key,
+    from_Language varchar(32) not null,
+    to_Language varchar(32) not null,
+    n_questions tinyint not null,
+    n_options tinyint not null
+);
