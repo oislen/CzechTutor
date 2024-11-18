ceda20242988258c67f0d3018ff7871aa3ba2393
Oisin <oisin.leonard@gmail.com>
2024-11-18 19:57:08 +0000

    Updated post mapping for createQuizPayload

--- server/src/main/java/com/czechtutor/controller/ApplicationRestController.java
+++ server/src/main/java/com/czechtutor/controller/ApplicationRestController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.czechtutor.model.Question;
@@ -11,29 +12,22 @@ import com.czechtutor.service.QuizService;
 
 @RestController
 public class ApplicationRestController {
-
-    public String fromLanguage = "CZ";
-    public String toLanguage = "EN";
+    
     final public Integer nQuestions = 6;
     final public Integer nOptions = 4;
-
+    
     private final QuizService quizService;
-
+    
     public ApplicationRestController(QuizService quizService) {
         this.quizService = quizService;
     }
-
+    
     @PostMapping("/home")
-    //public Photo create(@RequestPart("data") MultipartFile file) throws IOException {
-    public ArrayList<Question> createQuizPayload() {
-        // create a lesson payload
-        HashMap<String, Object> lessonPayload = new HashMap<>();
-        lessonPayload.put("fromLanguage", fromLanguage);
-        lessonPayload.put("toLanguage", toLanguage);
-        lessonPayload.put("nQuestions", nQuestions);
-        lessonPayload.put("nOptions", nOptions);
+    public ArrayList<Question> createQuizPayload(@RequestBody HashMap<String, Object> payload) {
+        payload.put("nQuestions", nQuestions);
+        payload.put("nOptions", nOptions);
         // generate a quiz
-        ArrayList<Question> quiz = quizService.create(lessonPayload);
+        ArrayList<Question> quiz = quizService.create(payload);
         return quiz;
     }
 }
