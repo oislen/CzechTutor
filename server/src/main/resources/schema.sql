14539c49b581d55781bf50de2360b75bbbd1d251
Oisin <oisin.leonard@gmail.com>
2024-11-18 12:32:50 +0000

    auto incrementing primary key

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 create table if not exists lesson (
-    lesson_id bigint primary key,
+    lesson_id bigint auto_increment primary key,
     from_Language varchar(32) not null,
     to_Language varchar(32) not null,
     n_questions tinyint not null,
