1b7da5e8e059445c5a4b027effc4b7f383eea33d
Oisin <oisin.leonard@gmail.com>
2024-11-19 21:44:05 +0000

    Split out results.js from results.html

---
+++ server/src/main/resources/static/js/src/results.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    document.getElementById('option1').value = 'Option 1';
+    document.getElementById('option2').value = 'Option 2';
+    document.getElementById('option3').value = 'Option 3';
+    document.getElementById('option4').value = 'Option 4';
+  };
