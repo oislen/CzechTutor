dfe9677459c0c6e8e8ce803d35a0dc817cf44004
Oisin <oisin.leonard@gmail.com>
2024-11-20 08:53:29 +0000

    Combined Controller and RestController into one

--- server/src/main/java/com/czechtutor/controller/ApplicationController.java
+++ server/src/main/java/com/czechtutor/controller/ApplicationController.java
@@ -1,22 +1,63 @@
 package com.czechtutor.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.czechtutor.model.Question;
+import com.czechtutor.service.QuizService;
 
 @Controller
 public class ApplicationController {
+     
+    final public Integer nQuestions = 6;
+    final public Integer nOptions = 4;
     
-    @GetMapping("/home")
+    private final QuizService quizService;
+    
+    public ApplicationController(QuizService quizService) {
+        this.quizService = quizService;
+    }
+           
+    @GetMapping(path="/")
+    public String redirectHomePage() {
+        return "redirect:/home";
+    }
+
+    @GetMapping(path="/home")
     public String getHomePage() {
         return "home";
     }
 
-    @GetMapping("/lesson")
+    @PostMapping(path="/home", consumes="application/json")
+    @ResponseBody
+    public ArrayList<Question> createQuizPayload(@RequestBody HashMap<String, Object> payload) {
+        payload.put("nQuestions", nQuestions);
+        payload.put("nOptions", nOptions);
+        // generate a quiz
+        ArrayList<Question> quiz = quizService.create(payload);
+        return quiz;
+    }
+
+    @GetMapping(path="/lesson")
     public String getLessonPage() {
         return "lesson";
     }
 
-    @GetMapping("/results")
+    @PostMapping(path="/lesson", consumes="application/json")
+    @ResponseBody
+    public Integer createResultsPayload(@RequestBody ArrayList<HashMap<String,Object>> payload) {
+        // calculate results
+        Integer totalCorrect = quizService.countTotalCorrect(payload);
+        return totalCorrect;
+    }
+
+    @GetMapping(path="/results")
     public String getResultsPage() {
         return "results";
     }
