9c004c9ffd14dc9b9ab755e171d135f34a27d95b
Oisin <oisin.leonard@gmail.com>
2024-11-25 09:01:47 +0000

    Added getters and setters for model attributes

--- server/src/main/java/com/czechtutor/model/ResultModel.java
+++ server/src/main/java/com/czechtutor/model/ResultModel.java
@@ -19,20 +19,36 @@ public class ResultModel {
 	//@NotEmpty
 	private Integer result;
 	
-    public void set(HashMap<String,Object> payload) {
+  public void set(HashMap<String,Object> payload) {
 		// set class objects
 		//this.resultId = (Integer) payload.get("resultId");
 		this.lessonId = (Integer) payload.get("lessonId");
 		this.result = (Integer) payload.get("result");
-    }
+  }
  
-    public HashMap<String,Object> getResultPayload() {
-        // create results payload
-        HashMap<String,Object> resultsPayload = new HashMap<>();
-        resultsPayload.put("resultId", resultId);
-        resultsPayload.put("lessonId", lessonId);
-        resultsPayload.put("result", result);
-        return resultsPayload;
-    }
+  public HashMap<String,Object> getResultPayload() {
+    // create results payload
+    HashMap<String,Object> resultsPayload = new HashMap<>();
+    resultsPayload.put("resultId", resultId);
+    resultsPayload.put("lessonId", lessonId);
+    resultsPayload.put("result", result);
+    return resultsPayload;
+  }
+
+  public Integer getLessonId(){
+    return lessonId;
+  }
+    
+  public void setLessonId(Integer lessonId){
+    this.lessonId = lessonId;
+  }
+  
+  public Integer getResult(){
+    return result;
+  }
+    
+  public void setResult(Integer result){
+    this.result = result;
+  }
 
 }
