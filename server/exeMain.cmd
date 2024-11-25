443b331cd70d4455d21e0bbebca517580307425b
Oisin <oisin.leonard@gmail.com>
2024-11-25 09:17:37 +0000

    Removed execute Main.cmd

--- server/exeMain.cmd
+++
@@ -1,8 +0,0 @@
-:: call javac src\\main\\java\\com\\czechtutor\\Main.java
-:: call java -cp src\\main\\java com.czechtutor.Main
-call mvnw exec:java -Dexec.mainClass="com.czechtutor.Main"
-:: call mvnw compile test
-:: call mvnw clean compile test package
-:: call mvnw clean install
-:: call mvnw spring-boot:run
-:: call mvn clean install -U
