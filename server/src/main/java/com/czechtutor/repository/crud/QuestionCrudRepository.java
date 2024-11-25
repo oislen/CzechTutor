67cd492e3bff323612e11b5fc1e654c1c2d1d249
Oisin <oisin.leonard@gmail.com>
2024-11-25 07:57:38 +0000

    Added new method findByLessonId

--- server/src/main/java/com/czechtutor/repository/crud/QuestionCrudRepository.java
+++ server/src/main/java/com/czechtutor/repository/crud/QuestionCrudRepository.java
@@ -1,8 +1,13 @@
 package com.czechtutor.repository.crud;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.czechtutor.model.QuestionModel;
 
 public interface QuestionCrudRepository extends CrudRepository<QuestionModel, Integer> {
+    
+    ArrayList<QuestionModel> findByLessonId(String LessonId);
+
 }
