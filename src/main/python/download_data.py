daa38a6bb7417b5a8895003374f8aebe095e5064
Oisin <oisin.leonard@gmail.com>
2024-11-30 11:32:56 +0000

    Minor schange to head

---
+++ src/main/python/download_data.py
@@ -0,0 +1,24 @@
+
+import os
+import requests
+import zipfile
+import io
+import pandas as pd
+# set destination directory
+destination_directory="E:\\GitHub\\CzechTutor\\src\\main\\resources\\data"
+if False:
+    # set source url and 
+    zip_file_url="https://www.manythings.org/anki/ces-eng.zip"
+    # download and extract files
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    r = requests.get(zip_file_url, headers=headers)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(destination_directory)
+# load text data
+data = pd.read_csv(os.path.join(destination_directory, "ces.txt"), sep="\t", names=["EN","CZ","REF"], header=None)
+# create ID column
+data['ID'] = data.index+1
+# set column order
+data = data[["ID","EN","CZ","REF"]]
+# save data as .csv file
+data.to_csv(os.path.join(destination_directory, "ces.csv"), index=False)
