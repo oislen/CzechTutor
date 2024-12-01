29d898a394f146ec86f26eb448d0c1ec5f394918
Oisin <oisin.leonard@gmail.com>
2024-12-01 11:06:52 +0000

    Added findByLevel method to ces crud repo

--- src/main/java/com/czechtutor/repository/crud/CesCrudRepository.java
+++ src/main/java/com/czechtutor/repository/crud/CesCrudRepository.java
@@ -1,8 +1,13 @@
 package com.czechtutor.repository.crud;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.czechtutor.model.CesModel;
 
 public interface CesCrudRepository extends CrudRepository<CesModel, Integer> {
+
+        ArrayList<CesModel> findByLevel(String Level);
+
 }
