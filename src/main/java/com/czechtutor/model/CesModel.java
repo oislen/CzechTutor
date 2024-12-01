db61ebedd22d1e6c5dd0ed7064776aa98f1b1a67
Oisin <oisin.leonard@gmail.com>
2024-12-01 11:06:04 +0000

    Added getters and setters. Added attribute for difficulty level

--- src/main/java/com/czechtutor/model/CesModel.java
+++ src/main/java/com/czechtutor/model/CesModel.java
@@ -11,32 +11,69 @@ import org.springframework.data.relational.core.mapping.Table;
 public class CesModel {
 	
 	@Id
-	private Integer ID;
-	
-	//@NotEmpty
-	private String EN;
-	
-	//@NotEmpty
-	private String CZ;
-	
-    //@NotEmpty
-    private String REF;
-
+	private Integer id;
+	private String english;
+	private String czech;
+    private String level;
+    private String reference;
+    
     public void set(HashMap<String,Object>  payload){
         // set the class objects
-		//this.ID = (Integer) payload.get("ID");
-		this.EN = (String) payload.get("EN");
-        this.CZ = (String) payload.get("CZ");
-        this.REF = (String) payload.get("REF");
+		//this.id = (Integer) payload.get("id");
+		this.english = (String) payload.get("english");
+        this.czech = (String) payload.get("czech");
+        this.level = (String) payload.get("level");
+        this.reference = (String) payload.get("reference");
     }
 	
     public HashMap<String,Object> getCesPayload(){
 	    // construct questionPayload
         HashMap<String,Object> questionPayload = new HashMap<>();
-        questionPayload.put("ID", ID);
-        questionPayload.put("EN", EN);
-        questionPayload.put("CZ", CZ);
-        questionPayload.put("REF", REF);
+        questionPayload.put("id", id);
+        questionPayload.put("english", english);
+        questionPayload.put("czech", czech);
+        questionPayload.put("level", level);
+        questionPayload.put("reference", reference);
         return questionPayload;
     }
+    
+    public Integer getId(){
+		return id;
+	}
+	
+    public void setId(Integer id){
+		this.id = id;
+	}
+    
+    public String getEnglish(){
+		return english;
+	}
+	
+    public void setEnglish(String english){
+		this.english = english;
+	}
+    
+    public String getCzech(){
+		return czech;
+	}
+	
+    public void setCzech(String czech){
+		this.czech = czech;
+	}
+    
+    public String getLevel(){
+		return level;
+	}
+	
+    public void setLevel(String level){
+		this.level = level;
+	}
+    
+    public String getReference(){
+		return reference;
+	}
+	
+    public void setRef(String reference){
+		this.reference = reference;
+	}
 }
