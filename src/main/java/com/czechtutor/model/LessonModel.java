589fa1c49eda3ac8d3f18ca0d7ea7870ee003d9f
Oisin <oisin.leonard@gmail.com>
2024-12-03 08:16:39 +0000

    Added javadocs and formatted with java syntax formatteer

--- src/main/java/com/czechtutor/model/LessonModel.java
+++ src/main/java/com/czechtutor/model/LessonModel.java
@@ -5,32 +5,50 @@ import java.util.HashMap;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
-//import javax.validation.constraints.NotEmpty;
-
+/**
+ * <p>
+ * The lesson model class defines the schema / structure of a lesson</p>
+ *
+ * @author oislen
+ */
 @Table("LESSONS")
 public class LessonModel {
-	
-	@Id
-	private Integer lessonId;
-	private String fromLanguage;
-	private String toLanguage;
-	private Integer nQuestions;
-	private Integer nOptions;
-	private String level;
 
-    public void set(HashMap<String,Object>  payload){
+    @Id
+    private Integer lessonId;
+    private String fromLanguage;
+    private String toLanguage;
+    private Integer nQuestions;
+    private Integer nOptions;
+    private String level;
+
+    /**
+     * <p>
+     * Sets the attributes of a lesson model using a hashmap of attribute
+     * values</p>
+     *
+     * @param payload a hashmap of attributes to set the lesson model with
+     */
+    public void set(HashMap<String, Object> payload) {
         // set the class objects
-		//this.lessonId = (Integer) payload.get("lessonId");
-		this.fromLanguage = (String) payload.get("fromLanguage");
-		this.toLanguage = (String) payload.get("toLanguage");
-		this.nQuestions = (Integer) payload.get("nQuestions");
-		this.nOptions = (Integer) payload.get("nOptions");
-		this.level = (String) payload.get("level");
+        //this.lessonId = (Integer) payload.get("lessonId");
+        this.fromLanguage = (String) payload.get("fromLanguage");
+        this.toLanguage = (String) payload.get("toLanguage");
+        this.nQuestions = (Integer) payload.get("nQuestions");
+        this.nOptions = (Integer) payload.get("nOptions");
+        this.level = (String) payload.get("level");
     }
 
-    public HashMap<String,Object> getLessonPayload(){
-	    // construct lesson payload
-        HashMap<String,Object> lessonPayload = new HashMap<>();
+    /**
+     * <p>
+     * Gets the attributes of a lesson model as a hashmap of attribute
+     * values</p>
+     *
+     * @return a hashmap of the lesson model attributes
+     */
+    public HashMap<String, Object> getLessonPayload() {
+        // construct lesson payload
+        HashMap<String, Object> lessonPayload = new HashMap<>();
         lessonPayload.put("lessonId", lessonId);
         lessonPayload.put("fromLanguage", fromLanguage);
         lessonPayload.put("toLanguage", toLanguage);
@@ -38,54 +56,126 @@ public class LessonModel {
         lessonPayload.put("nOptions", nOptions);
         lessonPayload.put("level", level);
         return lessonPayload;
-	}
+    }
 
-    public Integer getLessonId(){
-		return lessonId;
-	}
-	
-    public void setLessonId(Integer lessonId){
-		this.lessonId = lessonId;
-	}
+    /**
+     * <p>
+     * Gets the lesson id attribute of a lesson model</p>
+     *
+     * @returns the lesson id attribute
+     */
+    public Integer getLessonId() {
+        return lessonId;
+    }
 
-    public String getFromLanguage(){
-		return fromLanguage;
-	}
-	
-    public void setFromLanguage(String fromLanguage){
-		this.fromLanguage = fromLanguage;
-	}
+    /**
+     * <p>
+     * Sets the lesson id attribute of a lesson model</p>
+     *
+     * @param lessonId the lesson id attribute
+     */
+    public void setLessonId(Integer lessonId) {
+        this.lessonId = lessonId;
+    }
 
-    public String getToLanguage(){
-		return toLanguage;
-	}
-	
-    public void setToLanguage(String toLanguage){
-		this.toLanguage = toLanguage;
-	}
+    /**
+     * <p>
+     * Gets the lesson from language attribute of a lesson model</p>
+     *
+     * @returns the lesson from language attribute
+     */
+    public String getFromLanguage() {
+        return fromLanguage;
+    }
 
-    public Integer getNQuestions(){
-		return nQuestions;
-	}
-	
-    public void setNQuestions(Integer nQuestions){
-		this.nQuestions = nQuestions;
-	}
+    /**
+     * <p>
+     * Sets the lesson from language attribute of a lesson model</p>
+     *
+     * @param fromLanguage the lesson from language attribute
+     */
+    public void setFromLanguage(String fromLanguage) {
+        this.fromLanguage = fromLanguage;
+    }
 
-    public Integer getNOptions(){
-		return nOptions;
-	}
-	
-    public void setNOptions(Integer nOptions){
-		this.nOptions = nOptions;
-	}
+    /**
+     * <p>
+     * Gets the lesson to language attribute of a lesson model</p>
+     *
+     * @returns the lesson to language attribute
+     */
+    public String getToLanguage() {
+        return toLanguage;
+    }
 
-    public String getLevel(){
-		return level;
-	}
-	
-    public void setLevel(String level){
-		this.level = level;
-	}
+    /**
+     * <p>
+     * Sets the lesson to language attribute of a lesson model</p>
+     *
+     * @param toLanguage the lesson to language attribute
+     */
+    public void setToLanguage(String toLanguage) {
+        this.toLanguage = toLanguage;
+    }
+
+    /**
+     * <p>
+     * Gets the lesson n questions attribute of a lesson model</p>
+     *
+     * @returns the lesson n questions attribute
+     */
+    public Integer getNQuestions() {
+        return nQuestions;
+    }
+
+    /**
+     * <p>
+     * Sets the lesson n questions attribute of a lesson model</p>
+     *
+     * @param nQuestions the lesson n questions attribute
+     */
+    public void setNQuestions(Integer nQuestions) {
+        this.nQuestions = nQuestions;
+    }
+
+    /**
+     * <p>
+     * Gets the lesson n options attribute of a lesson model</p>
+     *
+     * @returns the lesson n options attribute
+     */
+    public Integer getNOptions() {
+        return nOptions;
+    }
+
+    /**
+     * <p>
+     * Sets the lesson n options attribute of a lesson model</p>
+     *
+     * @param nOptions the lesson n options attribute
+     */
+    public void setNOptions(Integer nOptions) {
+        this.nOptions = nOptions;
+    }
+
+    /**
+     * <p>
+     * Gets the lesson level attribute of a lesson model</p>
+     *
+     * @returns the level options attribute
+     */
+    public String getLevel() {
+        return level;
+    }
+
+    /**
+     * <p>
+     * Sets the lesson level attribute of a lesson model</p>
+     *
+     * @param level the lesson level attribute
+     */
+    public void setLevel(String level) {
+        this.level = level;
+    }
 
 }
