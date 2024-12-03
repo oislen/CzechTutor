4365bc646f7759e1989464052dd2754b8bb68bbe
Oisin <oisin.leonard@gmail.com>
2024-12-03 08:27:30 +0000

    Formatted java scripts with java syntax formatter, added javadocs to classes and methods

--- src/main/java/com/czechtutor/repository/crud/AnswerCrudRepository.java
+++ src/main/java/com/czechtutor/repository/crud/AnswerCrudRepository.java
@@ -6,8 +6,22 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.czechtutor.model.AnswerModel;
 
+/**
+ * <p>
+ * The answer crud repository class defines the available processes to interact
+ * with the answer table with</p>
+ *
+ * @author oislen
+ */
 public interface AnswerCrudRepository extends CrudRepository<AnswerModel, Integer> {
 
+    /**
+     * <p>
+     * Finds all answer models with a specified lesson id as an array list</p>
+     *
+     * @param LessonId the lesson id to find by
+     * @returns the answer models as an array list
+     */
     ArrayList<AnswerModel> findByLessonId(String LessonId);
 
 }
