b91bc7630d1d626c2201e239955a8effe0ab2d5e
Oisin <oisin.leonard@gmail.com>
2025-06-14 16:10:47 +0100

    Extract the custom logic for lesson results and lesson quesitons and answers

--- src/main/java/com/czechtutor/repository/crud/ResultCrudRepository.java
+++ src/main/java/com/czechtutor/repository/crud/ResultCrudRepository.java
@@ -1,15 +1,8 @@
 package com.czechtutor.repository.crud;
 
-import java.util.ArrayList;
-
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
-import com.czechtutor.model.LessonModel;
 import com.czechtutor.model.ResultModel;
-import com.czechtutor.model.custom.LessonQuestionAnswer;
-import com.czechtutor.model.custom.LessonResult;
 
 /**
  * <p>
@@ -29,29 +22,4 @@ public interface ResultCrudRepository extends CrudRepository<ResultModel, Intege
      */
     ResultModel findByLessonId(String LessonId);
 
-    public static final String LESSON_QUESTIONS_ANSWERS_QUERY = """
-    select
-        l.lesson_id, l.from_Language, l.to_Language, l.n_questions, l.n_options, l.level,
-        q.question_id, q.phrase, q.option1, q.option2, q.option3, q.option4, q.solution,
-        a.answer_id, a.answer, a.correct
-    from lessons as l
-    inner join questions as q on l.lesson_id = q.lesson_id
-    inner join answers as a on q.question_id = a.question_id
-    where l.lesson_id = :#{#lessonModel.LessonId}
-    """;
-
-    @Query(LESSON_QUESTIONS_ANSWERS_QUERY)
-    ArrayList<LessonQuestionAnswer> findAllQuestionsAnswersByLesson(@Param("lessonModel") LessonModel lessonModel);
-
-    public static final String LESSONS_RESULTS_QUERY = """
-    select 
-        l.lesson_id, l.from_Language, l.to_Language, l.n_questions, l.n_options, l.level,
-        r.result_id, r.n_correct, r.score
-    from lessons as l
-    inner join results as r on l.lesson_id = r.lesson_id
-    ;
-    """;
-
-    @Query(LESSONS_RESULTS_QUERY)
-    ArrayList<LessonResult> findAllLessonsResults();
 }
