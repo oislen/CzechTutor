3cf4ababff4d2bdc5342eb5f33ac3eff1ed1e4cf
Oisin <oisin.leonard@gmail.com>
2025-06-14 14:38:38 +0100

    Applied while loop to find distinct random indices. Removed hashmap logic and directly generating question model from ces model

--- src/main/java/com/czechtutor/service/QuestionService.java
+++ src/main/java/com/czechtutor/service/QuestionService.java
@@ -1,7 +1,6 @@
 package com.czechtutor.service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Random;
 
 import org.springframework.stereotype.Service;
@@ -60,46 +59,46 @@ public class QuestionService {
      */
     public QuestionModel create(LessonModel lessonModel, Long randomSeed) {
         // set question
-        ArrayList<CesModel> cesModelArray = cesCrudRepository.findByLevel(lessonModel.getLevel());
-        Integer upperIndexBound = (int) cesModelArray.size();
+        ArrayList<CesModel> cesModelLevelArray = cesCrudRepository.findByLevel(lessonModel.getLevel());
+        Integer upperIndexBound = (int) cesModelLevelArray.size();
         // create random generator and set seed if required
         Random randomGenerator = new Random();
         if (randomSeed != null) {
             randomGenerator.setSeed(randomSeed);
         }
         // randomly select nOptions to extract from the record set (between 0 and upperIndexBound)
+        Boolean findRandomIndices = true;
         ArrayList<Integer> indexArray = new ArrayList<>();
-        for (int i = 0; i < lessonModel.getNOptions(); i++) {
+        while (findRandomIndices) {
             Integer index = randomGenerator.nextInt(upperIndexBound);
-            indexArray.add(index);
+            if (!indexArray.contains(index)) {
+                indexArray.add(index);
+                if (indexArray.size() == lessonModel.getNOptions()) {
+                    findRandomIndices = false;
+                }
+            }
         }
         // select the records corresponding to the random indices 
-        ArrayList<HashMap<String, Object>> filteredRecordSet = new ArrayList<>();
-        for (int i = 1; i <= upperIndexBound; i++) {
-            if (indexArray.contains(i)) {
-                CesModel cesModel = cesModelArray.get(i);
-                HashMap<String, Object> cesPayload = cesModel.getCesPayload();
-                filteredRecordSet.add(cesPayload);
-            }
+        ArrayList<CesModel> cesModelArray = new ArrayList<>();
+        for (Integer index : indexArray) {
+                CesModel cesModel = cesModelLevelArray.get(index);
+                cesModelArray.add(cesModel);
         }
         // randomly determine the phase, answer and options
         Integer phaseIndex = randomGenerator.nextInt(lessonModel.getNOptions());
-        ArrayList<Object> optionsArray = new ArrayList<>();
-        for (HashMap<String, Object> hashMapObject : filteredRecordSet) {
-            optionsArray.add(hashMapObject.get(lessonModel.getToLanguage()));
+        ArrayList<String> optionsArray = new ArrayList<>();
+        for (CesModel cesModel : cesModelArray) {
+            optionsArray.add((String) cesModel.getCesPayload().get(lessonModel.getToLanguage()));
         }
-        // create question payload
-        HashMap<String, Object> lessonPayload;
-        lessonPayload = lessonModel.getLessonPayload();
-        lessonPayload.put("phrase", filteredRecordSet.get(phaseIndex).get(lessonModel.getFromLanguage()));
-        lessonPayload.put("option1", optionsArray.get(0));
-        lessonPayload.put("option2", optionsArray.get(1));
-        lessonPayload.put("option3", optionsArray.get(2));
-        lessonPayload.put("option4", optionsArray.get(3));
-        lessonPayload.put("solution", filteredRecordSet.get(phaseIndex).get(lessonModel.getToLanguage()));
         // create a question
         QuestionModel questionModel = new QuestionModel();
-        questionModel.set(lessonPayload);
+        questionModel.setLessonId(lessonModel.getLessonId());
+        questionModel.setPhrase((String) cesModelArray.get(phaseIndex).getCesPayload().get(lessonModel.getFromLanguage()));
+        questionModel.setOption1(optionsArray.get(0));
+        questionModel.setOption2(optionsArray.get(1));
+        questionModel.setOption3(optionsArray.get(2));
+        questionModel.setOption4(optionsArray.get(3));
+        questionModel.setSolution((String) cesModelArray.get(phaseIndex).getCesPayload().get(lessonModel.getToLanguage()));
         questionCrudRepository.save(questionModel);
         return questionModel;
     }
