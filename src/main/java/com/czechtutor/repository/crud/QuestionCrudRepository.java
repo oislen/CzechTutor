89109b594f350d0db386ff3a87fd19f8fe24b6d4
Oisin <oisin.leonard@gmail.com>
2025-06-15 12:12:57 +0100

    Added custom sql query to pull question using lesson id and question lesson id

--- src/main/java/com/czechtutor/repository/crud/QuestionCrudRepository.java
+++ src/main/java/com/czechtutor/repository/crud/QuestionCrudRepository.java
@@ -2,7 +2,9 @@ package com.czechtutor.repository.crud;
 
 import java.util.ArrayList;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.czechtutor.model.QuestionModel;
 
@@ -23,5 +25,12 @@ public interface QuestionCrudRepository extends CrudRepository<QuestionModel, In
      * @return the question models as an array list
      */
     ArrayList<QuestionModel> findByLessonId(String LessonId);
+
+    public static final String GET_QUESTION_BY_LESSON_QUESTION_IDS_QUERY = """
+    select * from questions where lesson_id = :#{#lessonId.intValue()} and lesson_question_id = :#{#LessonQuestionId.intValue()}
+    """;
+
+    @Query(GET_QUESTION_BY_LESSON_QUESTION_IDS_QUERY)
+    QuestionModel getQuestionByLessonQuestionIds(@Param("lessonId") Integer lessonId, @Param("LessonQuestionId") Integer LessonQuestionId);
 
 }
